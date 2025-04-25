ca4c7ce229991ad3ea9879255727ebdf64f64c69
Leo Tong <leo.tong@student.unsw.edu.au>
2025-04-25 20:30:23 +1000

    added about page route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('about', function () {
+    return Inertia::render('About');
+})->name('about');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
 
