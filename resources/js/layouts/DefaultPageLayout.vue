3f11e20134a47156ed4f7c5a2e55b74c0fbbf037
Leo Tong <leo.tong@student.unsw.edu.au>
2025-04-26 23:08:15 +1000

    added default layout for pages with text

--- resources/js/layouts/DefaultPageLayout.vue
+++ resources/js/layouts/DefaultPageLayout.vue
@@ -1,13 +1,62 @@
-<script setup lang=ts>
+<script setup lang="ts">
+import DefaultFooter from '@/components/DefaultFooter.vue';
+import DefaultMain from '@/components/DefaultMain.vue';
+import DefaultNavBar from '@/components/DefaultNavBar.vue';
+import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
+import type { BreadcrumbItemType } from '@/types';
+
+interface Props {
+    breadcrumbs?: BreadcrumbItemType[];
+}
+
+withDefaults(defineProps<Props>(), {
+    breadcrumbs: () => [],
+});
 </script>
 
 <template>
-    <div class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
-
-        <main></main>
-        
-    </div>
+    <DefaultNavBar :breadcrumbs="breadcrumbs" />
+    <DefaultMain>
+        <slot>
+            <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+                    <div
+                        class="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border"
+                    >
+                        <PlaceholderPattern />
+                    </div>
+                    <div
+                        class="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border"
+                    >
+                        <PlaceholderPattern />
+                    </div>
+                    <div
+                        class="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border"
+                    >
+                        <PlaceholderPattern />
+                    </div>
+                </div>
+                <div
+                    class="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min"
+                >
+                    <PlaceholderPattern />
+                </div>
+            </div>
+        </slot>
+    </DefaultMain>
+    <DefaultFooter />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
+
+<!--
+    Defines the default layout for most of the pages.
+
+    Follows the
+
+    header
+    main
+    footer
+
+    structure.
+-->
---
+++ resources/js/layouts/TextPageLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="mx-auto w-full max-w-4xl px-4 py-12">
+        <slot>
+            Write some text here
+        </slot>
+    </div>
+</template>
+
+<style scoped></style>
+
+<!--
+    Defines the structure for routes that are mainly text based
+-->
