a2e6e8b9676fc475ccf0cf90b24d439d3988c4c7
Leo Tong <leo.tong@student.unsw.edu.au>
2025-04-26 23:08:25 +1000

    added some static pages

---
+++ resources/js/pages/Contact.vue
@@ -0,0 +1,29 @@
+<script setup lang=ts>
+import DefaultPageLayout from '@/layouts/DefaultPageLayout.vue';
+import { BreadcrumbItemType } from '@/types';
+
+const breadcrumbs: BreadcrumbItemType[] = [
+    {
+        title: 'Home',
+        href: '/'
+    },
+    {
+        title: 'Contact',
+        href: '/contact'
+    },
+]
+
+</script>
+
+<template>
+    <DefaultPageLayout :breadcrumbs="breadcrumbs">
+
+    </DefaultPageLayout>
+</template>
+
+<style scoped>
+</style>
+
+<!--
+    View for the contact page with a contact form to ask question and stuff
+-->
---
+++ resources/js/pages/RoadMap.vue
@@ -0,0 +1,29 @@
+<script setup lang=ts>
+import DefaultPageLayout from '@/layouts/DefaultPageLayout.vue';
+import { BreadcrumbItemType } from '@/types';
+
+const breadcrumbs: BreadcrumbItemType[] = [
+    {
+        title: 'Home',
+        href: '/'
+    },
+    {
+        title: 'Road Map',
+        href: '/roadmap'
+    },
+]
+
+</script>
+
+<template>
+    <DefaultPageLayout :breadcrumbs="breadcrumbs">
+
+    </DefaultPageLayout>
+</template>
+
+<style scoped>
+</style>
+
+<!--
+    View for the roadmap page that outlines the roadmap for the site
+-->
