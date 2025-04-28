bf6060da6462e39caf43b7a841d5bd1c5ae9852c
Leo Tong <leo.tong@student.unsw.edu.au>
2025-04-28 23:46:52 +1000

    added routes for contact page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return Inertia::render('Home');
 })->name('home');
 
 Route::get('dashboard', function () {
@@ -15,6 +15,11 @@ Route::get('about', function () {
     return Inertia::render('About');
 })->name('about');
 
+Route::get('roadmap', function () {
+    return Inertia::render('RoadMap');
+})->name('roadmap');
+
+require __DIR__.'/contact.php';
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
 
