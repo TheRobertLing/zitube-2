12ae0c3b58f2c0e1c995397d5f3c6e427e8b6418
Leo Tong <leo.tong@student.unsw.edu.au>
2025-05-17 01:30:51 +1000

    styled scrollbar

--- resources/js/pages/dev/VideoSideBar.vue
+++ resources/js/pages/dev/VideoSideBar.vue
@@ -10,9 +10,9 @@ defineProps<{
 </script>
 
 <template>
-    <div class="gutter flex h-full items-center justify-center overflow-y-auto p-4">
+    <div class="gutter scrollable-content flex h-full items-center justify-center p-4">
         <Tabs default-value="Transcript" class="h-full w-full">
-            <TabsList class="grid grid-flow-col auto-cols-fr w-full">
+            <TabsList class="grid w-full auto-cols-fr grid-flow-col">
                 <template v-for="(item, index) in videoTabItems" :key="index">
                     <TabsTrigger :value="item.tabName" class="cursor-pointer"> {{ item.tabName }} </TabsTrigger>
                 </template>
@@ -38,6 +38,31 @@ defineProps<{
 <style scoped>
 .gutter {
     scrollbar-gutter: stable;
+}
+
+.scrollable-content {
+    overflow-y: auto;
+    scrollbar-width: thin;
+    scrollbar-color: #888 transparent;
+}
+
+.scrollable-content::-webkit-scrollbar {
+    width: 8px;
+}
+
+.scrollable-content::-webkit-scrollbar-track {
+    background: transparent;
+}
+
+.scrollable-content::-webkit-scrollbar-thumb {
+    background-color: #555;
+    border-radius: 4px;
+    border: 2px solid transparent;
+    background-clip: content-box;
+}
+
+.scrollable-content::-webkit-scrollbar-thumb:hover {
+    background-color: #777;
 }
 </style>
 
