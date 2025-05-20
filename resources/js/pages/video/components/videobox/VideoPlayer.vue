01c0dff5cc10758db33db33bd22b0dc777ac05d7
Leo Tong <leo.tong@student.unsw.edu.au>
2025-05-20 15:01:18 +1000

    feat(video): added stub views for prod video page

---
+++ resources/js/pages/video/components/videobox/VideoPlayer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { initVueYoutube } from '@/composables/useVueYoutube';
+import { useTemplateRef } from 'vue';
+
+const props = defineProps<{
+    videoId: string;
+}>();
+
+const videoFrameRef = useTemplateRef('videoContainer'); // YouTube container div
+initVueYoutube(props.videoId, videoFrameRef);
+</script>
+
+<template>
+    <div class="flex h-full items-center justify-center p-2">
+        <div ref="videoContainer"></div>
+    </div>
+</template>
+
+<!--
+    Video Player component
+-->
