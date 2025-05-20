2b861e08faa3f1eb517e904db3d0adbef2973a7e
Leo Tong <leo.tong@student.unsw.edu.au>
2025-05-21 02:04:22 +1000

    feat(layout): redefined layout for text based static pages

---
+++ resources/js/layouts/public/TextPageLayout.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import DefaultNavBar from '@/components/DefaultNavBar.vue';
+import type { BreadcrumbItemType } from '@/types';
+
+interface Props {
+    breadcrumbs?: BreadcrumbItemType[];
+}
+
+withDefaults(defineProps<Props>(), {
+    breadcrumbs: () => [],
+});
+</script>
+
+<template>
+    <div>
+      <DefaultNavBar :breadcrumbs="breadcrumbs" />
+
+      <main class="px-4 sm:px-6 lg:px-8 py-10">
+        <slot />
+      </main>
+    </div>
+  </template>
+
+
+<!--
+    Page layout for text based pages.
+-->
