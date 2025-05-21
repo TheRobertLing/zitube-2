c9bdd40ea1a5d238117d929de72a6b34b0ca2190
Leo Tong <leo.tong@student.unsw.edu.au>
2025-05-22 02:21:44 +1000

    refactor: adapted dev view into production view and added logic to better handle responsiveness

--- resources/js/pages/video/components/videosidebar/VideoSideBar.vue
+++ resources/js/pages/video/components/videosidebar/VideoSideBar.vue
@@ -2,7 +2,9 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { useDictionarySearch } from '@/composables/useDictionarySearch';
 import { TranscriptSegment, VideoMetaData, VideoTabItem } from '@/types';
-import { onMounted, ref } from 'vue';
+import { useElementSize } from '@vueuse/core';
+import { BookA, Captions, Info, Settings2 } from 'lucide-vue-next';
+import { onMounted, ref, useTemplateRef } from 'vue';
 import VideoDictionary from './sidebarcomponents/dictionary/VideoDictionary.vue';
 import VideoInfo from './sidebarcomponents/info/VideoInfo.vue';
 import VideoSettings from './sidebarcomponents/settings/VideoSettings.vue';
@@ -13,14 +15,11 @@ defineProps<{
     videoTranscriptData: TranscriptSegment[];
 }>();
 
+const isReady = ref<boolean>(false);
 const activeTab = ref<string>('Transcript');
-
 const { registerSearchCallback } = useDictionarySearch();
-
-onMounted(() => {
-    registerSearchCallback(() => {
-        activeTab.value = 'Dictionary';
-    });
+registerSearchCallback(() => {
+    activeTab.value = 'Dictionary';
 });
 
 const videoTabItems: VideoTabItem[] = [
@@ -28,56 +27,79 @@ const videoTabItems: VideoTabItem[] = [
         tabName: 'Info',
         cardTitle: 'Video Info',
         cardDescription: 'Overview, title, description, and upload details',
+        icon: Info,
         component: VideoInfo,
     },
     {
         tabName: 'Transcript',
         cardTitle: 'Transcript',
         cardDescription: 'View and interact with the transcript of the video here.',
+        icon: Captions,
         component: VideoTranscript,
     },
     {
         tabName: 'Dictionary',
         cardTitle: 'Dictionary',
         cardDescription: 'Look up definitions of words and phrases. Powered by CC-EDICT.',
+        icon: BookA,
         component: VideoDictionary,
     },
     {
         tabName: 'Settings',
         cardTitle: 'Settings',
         cardDescription: 'Adjust your preferences here.',
+        icon: Settings2,
         component: VideoSettings,
     },
 ];
+
+const tabsEl = useTemplateRef('tabs');
+const { width } = useElementSize(tabsEl);
+
+onMounted(() => {
+    if (width.value) {
+        isReady.value = true;
+    }
+});
 </script>
 
 <template>
-    <Tabs class="gutter h-full p-4" :unmount-on-hide="false" v-model:model-value="activeTab">
-        <TabsList class="grid w-full auto-cols-fr grid-flow-col">
-            <template v-for="(item, index) in videoTabItems" :key="index">
-                <TabsTrigger :value="item.tabName" class="cursor-pointer">
-                    {{ item.tabName }}
-                </TabsTrigger>
-            </template>
-        </TabsList>
+    <!-- Outer div is for the width watcher. Style MUST be kept for scrolling to work -->
+    <div ref="tabs" class="h-full w-full">
+        <Tabs class="h-full p-4" :unmount-on-hide="false" v-model:model-value="activeTab">
+            <TabsList class="grid w-full auto-cols-fr grid-flow-col">
+                <template v-for="(item, index) in videoTabItems" :key="index">
+                    <TabsTrigger :value="item.tabName" class="cursor-pointer overflow-hidden">
+                        <component :is="item.icon" class="h-4 w-4" />
 
-        <template v-for="(item, index) in videoTabItems" :key="index">
-            <TabsContent class="overflow-y-auto" :value="item.tabName">
-                <component
-                    :is="item.component"
-                    :video-tab-details="{
-                        cardTitle: item.cardTitle,
-                        cardDescription: item.cardDescription,
-                    }"
-                    :video-meta-data="videoMetaData"
-                    :video-transcript-data="videoTranscriptData"
-                />
-            </TabsContent>
-        </template>
-    </Tabs>
+                        <!-- Only show text if width is wide enough -->
+                        <span v-if="width > 500" class="truncate">
+                            {{ item.tabName }}
+                        </span>
+                    </TabsTrigger>
+                </template>
+            </TabsList>
+
+            <template v-for="(item, index) in videoTabItems" :key="index">
+                <!-- KEEP THIS OVERFLOW-Y-AUTO HERE IT BREAKS OTHERWISE -->
+                <TabsContent class="overflow-y-auto" :value="item.tabName">
+                    <component
+                        :is="item.component"
+                        :video-tab-details="{
+                            cardTitle: item.cardTitle,
+                            cardDescription: item.cardDescription,
+                        }"
+                        :video-meta-data="videoMetaData"
+                        :video-transcript-data="videoTranscriptData"
+                        :width="width"
+                    />
+                </TabsContent>
+            </template>
+        </Tabs>
+    </div>
 </template>
 
-<style scoped>
+<style>
 .gutter {
     scrollbar-gutter: stable;
 }
