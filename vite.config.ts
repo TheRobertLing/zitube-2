5452d3cc54a0ca19f4632319b52433cebab8f966
Leo Tong <leo.tong@student.unsw.edu.au>
2025-05-22 02:32:59 +1000

    refactor: removed rb custom component due to deprecation

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-import path from 'path';
-import tailwindcss from "@tailwindcss/vite";
 import { resolve } from 'node:path';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -18,9 +18,6 @@ export default defineConfig({
                 transformAssetUrls: {
                     base: null,
                     includeAbsolute: false,
-                },
-                compilerOptions: {
-                    isCustomElement: (tag) => ['rb'].includes(tag),
                 },
             },
         }),
