47281220b7b57186296cfe5cc210ccc6a7181a79
Leo Tong <leo.tong@student.unsw.edu.au>
2025-05-22 02:31:29 +1000

    chore(routes): add deprecation notice for about roadmap and contact routes

--- routes/public.php
+++ routes/public.php
@@ -7,12 +7,19 @@ Route::get('/', function () {
     return Inertia::render('Home');
 })->name('home');
 
+Route::get('/video', function () {
+    return Inertia::render('video/VideoPage');
+});
+
+/**
+ * Deprecate these, move these to vitepress documentation
+ */
 Route::get('about', function () {
-    return Inertia::render('About');
+    return Inertia::render('public/About');
 })->name('about');
 
 Route::get('roadmap', function () {
-    return Inertia::render('RoadMap');
+    return Inertia::render('public/RoadMap');
 })->name('roadmap');
 
 Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
