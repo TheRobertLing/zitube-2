9c30c9a8a3961eab645edf818d258fe8fd60b483
Leo Tong <leo.tong@student.unsw.edu.au>
2025-05-23 20:17:54 +1000

    feat: added loading screen to 'hide' visual jerk when useVueYoutube remove static width/height attributes

--- resources/js/pages/video/components/videobox/VideoPlayer.vue
+++ resources/js/pages/video/components/videobox/VideoPlayer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { initVueYoutube } from '@/composables/useVueYoutube';
+import { initVueYoutube, useVueYoutube } from '@/composables/useVueYoutube';
 import { useTemplateRef } from 'vue';
 
 const props = defineProps<{
@@ -8,13 +8,30 @@ const props = defineProps<{
 
 const videoFrameRef = useTemplateRef('videoContainer'); // YouTube container div
 initVueYoutube(props.videoId, videoFrameRef);
+const { isReady } = useVueYoutube();
 </script>
 
 <template>
-    <div class="flex h-full items-center justify-center p-2">
-        <div ref="videoContainer"></div>
+    <div ref="videoContainer" class="p-4"></div>
+    <div v-if="!isReady" class="bg-opacity-80 absolute inset-0 z-10 flex items-center justify-center p-4">
+        <div class="h-12 w-12 animate-spin rounded-full border-4 border-white border-t-transparent"></div>
     </div>
 </template>
+
+<style scoped>
+@keyframes loading-bar {
+    0% {
+        transform: translateX(-100%);
+    }
+    100% {
+        transform: translateX(100%);
+    }
+}
+
+.animate-loading-bar {
+    animation: loading-bar 1.2s infinite linear;
+}
+</style>
 
 <!--
     Video Player component
