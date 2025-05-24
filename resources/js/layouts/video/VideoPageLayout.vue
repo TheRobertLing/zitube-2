321aff70fd271e4205e01a440d72ae1f47407782
Leo Tong <leo.tong@student.unsw.edu.au>
2025-05-24 17:39:21 +1000

    style: updated height to reflect removal of breadcrumbs

--- resources/js/layouts/video/VideoPageLayout.vue
+++ resources/js/layouts/video/VideoPageLayout.vue
@@ -1,34 +1,15 @@
 <script setup lang="ts">
 import DefaultFooter from '@/components/DefaultFooter.vue';
 import DefaultNavBar from '@/components/DefaultNavBar.vue';
-import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
-import type { BreadcrumbItemType } from '@/types';
+import { useAttrs } from 'vue';
 
-interface Props {
-    breadcrumbs?: BreadcrumbItemType[];
-}
-
-withDefaults(defineProps<Props>(), {
-    breadcrumbs: () => [],
-});
+const attrs = useAttrs();
 </script>
 
 <template>
-    <DefaultNavBar :breadcrumbs="breadcrumbs" />
-    <main class="h-[calc(100vh-8rem)]">
-        <slot>
-            <ResizablePanelGroup
-                id="demo-group-1"
-                direction="horizontal"
-                class="max-h-[calc(100vh-8rem)] min-h-[calc(100vh-8rem)] w-full"
-            >
-                <ResizablePanel id="demo-panel-1" :default-size="50"> </ResizablePanel>
-
-                <ResizableHandle id="demo-handle-1" with-handle />
-
-                <ResizablePanel id="demo-panel-2" :default-size="50"> </ResizablePanel>
-            </ResizablePanelGroup>
-        </slot>
+    <DefaultNavBar />
+    <main v-bind="attrs" class="h-[calc(100vh-3.5em)]">
+        <slot></slot>
     </main>
     <DefaultFooter />
 </template>
